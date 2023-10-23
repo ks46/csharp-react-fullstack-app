5e63695187cfb0b5113e95602ac7aef8f47fc6f4
konas <konas@LAPTOP-1GHMR8NV>
2023-10-23 13:37:02 +0300

    updated Program.cs

--- back-end/courses.wwwapi/Program.cs
+++ back-end/courses.wwwapi/Program.cs
@@ -1,10 +1,16 @@
+using courses.wwwapi.Data;
 using courses.wwwapi.EndPoints;
+using courses.wwwapi.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddDbContext<DataContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
