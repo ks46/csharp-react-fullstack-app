db322e8f0a1c1035635171cb847be523588ddd53
konas <konas@LAPTOP-1GHMR8NV>
2023-10-23 13:36:50 +0300

    created DataContext

---
+++ back-end/courses.wwwapi/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using courses.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses.wwwapi.Data
+{
+    public class DataContext : DbContext
+    {
+        private string connectionString;
+
+        public DataContext()
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Declaration> Declarations { get; set; }
+
+    }
+}
