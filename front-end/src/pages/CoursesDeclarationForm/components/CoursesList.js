80306fe8eba92ae2b0424e54d06e160f361ffc7e
ks46 <konastaf46@gmail.com>
2023-10-27 13:09:01 +0300

    added css to checked courses

--- front-end/src/pages/CoursesDeclarationForm/components/CoursesList.js
+++ front-end/src/pages/CoursesDeclarationForm/components/CoursesList.js
@@ -1,23 +1,23 @@
 function Course({ course, isChecked, updateSelectedCourses }) {
   // TODO: display specializations for each course
   return (
-    <tr>
-      <td className='checkbox-table-col'>
+    <tr className={isChecked ? 'checked' : ''}>
+      <td className='table-col checkbox-table-col'>
         <input
           type='checkbox'
           checked={isChecked}
           onChange={() => updateSelectedCourses(course.id)}
         />
       </td>
-      <td className='course-table-col'>{course.name}</td>
-      <td className='ects-table-col'>{course.ects}</td>
-      <td className='type-table-col'>{course.description}</td>
-      <td className='s-table-col'>B</td>
-      <td className='s-table-col'></td>
-      <td className='s-table-col'>B</td>
-      <td className='s-table-col'>Y</td>
-      <td className='s-table-col'></td>
-      <td className='s-table-col'></td>
+      <td className='table-col course-table-col'>{course.name}</td>
+      <td className='table-col ects-table-col'>{course.ects}</td>
+      <td className='table-col type-table-col'>{course.description}</td>
+      <td className='table-col s-table-col'>B</td>
+      <td className='table-col s-table-col'></td>
+      <td className='table-col s-table-col'>B</td>
+      <td className='table-col s-table-col'>Y</td>
+      <td className='table-col s-table-col'></td>
+      <td className='table-col s-table-col'></td>
     </tr>
   );
 }
@@ -28,16 +28,16 @@ export default function CoursesList({ courses, selectedCourses, updateSelectedCo
       <table>
         <thead>
           <tr>
-            <th className='checkbox-table-col'></th>
-            <th className='course-table-col'>Course</th>
-            <th className='ects-table-col'>ECTS</th>
-            <th className='type-table-col'>Category</th>
-            <th className='s-table-col'>S1</th>
-            <th className='s-table-col'>S2</th>
-            <th className='s-table-col'>S3</th>
-            <th className='s-table-col'>S4</th>
-            <th className='s-table-col'>S5</th>
-            <th className='s-table-col'>S6</th>
+            <th className='table-col checkbox-table-col'></th>
+            <th className='table-col course-table-col'>Course</th>
+            <th className='table-col ects-table-col'>ECTS</th>
+            <th className='table-col type-table-col'>Category</th>
+            <th className='table-col s-table-col'>S1</th>
+            <th className='table-col s-table-col'>S2</th>
+            <th className='table-col s-table-col'>S3</th>
+            <th className='table-col s-table-col'>S4</th>
+            <th className='table-col s-table-col'>S5</th>
+            <th className='table-col s-table-col'>S6</th>
           </tr>
         </thead>
         <tbody>
