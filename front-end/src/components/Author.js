c565e13978fd6a592d07e53576d6b9a66a50b791
ks46 <konastaf46@gmail.com>
2023-10-31 14:49:10 +0200

    removed redundant front-end code

--- front-end/src/components/Author.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Author({ name, id }) {
-  return (
-    <Link to={`/profile/${id}`}>
-      <h4 className='author'>{name}</h4>
-    </Link>
-  )
-}
